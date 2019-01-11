70b3275a1705dfda022b6218eed30c16947957e5
OutrageousStop <ankitband@gmail.com>
2019-01-11 14:54:39 +0530

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import prime_generator
 import random
+import inverse
 
 def find_primitive_root(p):
     if p == 2:
@@ -15,8 +16,54 @@ def find_primitive_root(p):
 def key_generator():
     p = prime_generator.generate_prime_number()
     g = find_primitive_root(p)
-    a = random.randint(2, p - 2)
-    return {"p": p, "g": g, "a": a}
+    private = random.randint(2, p - 2)
+    public = pow(g, private, p)
+    return {"p": p, "private": private, "public": public, "gen": g}
 
+def ascii(message):
+    l = []
+    for i in message:
+        l.append(ord(i))
+    return l
+
+def tochar(message):
+    l = [chr(i) for i in message]
+    return "".join(l)
+
+def Encrypt(message, paras):
+    cipher = []
+    for i in message:
+        k = random.randint(2, paras['p'] - 2)
+        K = pow(paras['public'], k, paras['p'])
+        c1 = pow(paras['gen'], k, paras['p'])
+        c2 = (K * i) % paras['p']
+        cipher.append([c1, c2])
+    return cipher
+
+def Decrypt(ciphertext, paras):
+    dec = []
+    op = pow(ciphertext['c1'], paras['private'], paras['p'])
+        inv = inverse.modinv(op, paras['p'])
+        dec.append((ciphertext['c2'] * inverse.modinv(op, paras['p'])) % paras['p'])
+    return dec
 if __name__ == "__main__":
     paras = key_generator()
+    message = input('Enter the message: ')
+    message = ascii(message)
+    # dec = []
+    # for i in message:
+    #     k = random.randint(2, paras['p'] - 2)
+    #     K = pow(paras['public'], k, paras['p'])
+    #     c1 = pow(paras['gen'], k, paras['p'])
+    #     c2 = (K * i) % paras['p']
+        
+    #     # print('C1 = ', c1, '\n\n\nC2 = ', c2)
+
+    #     op = pow(c1, paras['private'], paras['p'])
+    #     inv = inverse.modinv(op, paras['p'])
+    #     # print("\n\n\n inv - ", inv)
+    #     dec.append((c2 * inverse.modinv(op, paras['p'])) % paras['p'])
+    #     # print("\n\n\n\n Plain - ", plain)
+    # print(tochar(dec))
+
+    
